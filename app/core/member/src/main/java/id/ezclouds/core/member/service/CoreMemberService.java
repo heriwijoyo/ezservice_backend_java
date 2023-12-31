e31bb52753f323768b772105be076fb08438115f
Heri Wijoyo <heri.wijoyo@gmail.com>
2023-12-31 08:34:47 +0700

    add initial core member code

---
+++ app/core/member/src/main/java/id/ezclouds/core/member/service/CoreMemberService.java
@@ -0,0 +1,15 @@
+/**
+ * Ezclouds.id
+ * Copyright (c) 2020‐2023 All Rights Reserved.
+ */
+package id.ezclouds.core.member.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Heri Wijoyo (heri.wijoyo@gmail.com)
+ * @version $Id: CoreMemberService.java, v 0.1 2023‐12‐31 7:35 AM Heri Wijoyo (heri.wijoyo@gmail.com) Exp $$
+ */
+@Service
+public class CoreMemberService {
+}
