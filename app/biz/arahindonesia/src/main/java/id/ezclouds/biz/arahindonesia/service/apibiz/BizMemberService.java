9083f1b6880d05103918d1aafcd1bf1b1414a026
Heri Wijoyo <heri.wijoyo@gmail.com>
2024-01-08 05:16:52 +0700

    minor refactor

--- app/biz/arahindonesia/src/main/java/id/ezclouds/biz/arahindonesia/service/apibiz/BizMemberService.java
+++ app/biz/arahindonesia/src/main/java/id/ezclouds/biz/arahindonesia/service/apibiz/BizMemberService.java
@@ -4,6 +4,7 @@
  */
 package id.ezclouds.biz.arahindonesia.service.apibiz;
 
+import id.ezclouds.biz.arahindonesia.model.member.BizMemberInfo;
 import id.ezclouds.biz.arahindonesia.service.core.MemberService;
 import id.ezclouds.biz.arahindonesia.service.request.BizMemberRegisterRequest;
 import id.ezclouds.biz.arahindonesia.service.result.BizResult;
@@ -11,8 +12,6 @@ import id.ezclouds.biz.arahindonesia.service.template.BizServiceTemplate;
 import id.ezclouds.common.util.assertion.AssertUtil;
 import id.ezclouds.common.util.error.EzErrorCode;
 import id.ezclouds.common.util.error.EzErrorException;
-import id.ezclouds.core.shared.context.EzAppContextHolder;
-import id.ezclouds.core.shared.service.CoreUniqueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,14 +22,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class BizMemberService {
 
-    private static final String DEFAULT_LOGIN_TYPE = "PHONE";
-
     @Autowired
     private MemberService memberService;
 
     public BizResult registerMember(BizMemberRegisterRequest request) throws EzErrorException {
         final BizResult bizResult = new BizResult();
-        final String orgId = EzAppContextHolder.getContext().getOrgId();
 
         BizServiceTemplate.execute(bizResult, new BizServiceTemplate.Handler() {
 
@@ -48,7 +44,8 @@ public class BizMemberService {
 
             @Override
             public void onBizProcess() throws EzErrorException {
-                bizResult.setObject(memberService.processRegisterMember(request));
+                BizMemberInfo bizMemberInfo = memberService.processRegisterMember(request);
+                bizResult.setObject(bizMemberInfo);
                 bizResult.setSuccess(true);
             }
         });
