176f0f1f5d89710f0cfdab5dfffbbd2df5c59657
Heri Wijoyo <heri.wijoyo@gmail.com>
2024-01-27 00:36:05 +0700

    initiate sample endpoint

--- app/core/bifrost/src/main/java/id/ezclouds/core/bifrost/app/api/ApiController.java
+++ app/core/bifrost/src/main/java/id/ezclouds/core/bifrost/app/api/ApiController.java
@@ -4,24 +4,14 @@
  */
 package id.ezclouds.core.bifrost.app.api;
 
-import id.ezclouds.biz.arahindonesia.model.AppSetting;
-import id.ezclouds.biz.arahindonesia.model.login.MemberLoginResult;
-import id.ezclouds.biz.arahindonesia.model.member.BizMemberInfo;
-import id.ezclouds.biz.arahindonesia.model.news.SimpleNews;
-import id.ezclouds.biz.arahindonesia.model.profile.CandidateProfile;
-import id.ezclouds.biz.arahindonesia.model.profile.MemberProfile;
-import id.ezclouds.biz.arahindonesia.service.core.CacheService;
 import id.ezclouds.core.bifrost.app.api.event.ApiEvent;
-import id.ezclouds.core.bifrost.app.api.request.ApiBaseRequest;
 import id.ezclouds.core.bifrost.app.api.request.ApiRequest;
-import id.ezclouds.core.bifrost.app.api.request.MemberLoginRequest;
-import id.ezclouds.core.bifrost.app.api.request.MemberRegisterRequest;
 import id.ezclouds.core.bifrost.app.api.result.ApiResult;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -31,51 +21,6 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/api", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
 public class ApiController extends AppController {
-
-    @PostMapping(value = "/appSetting.json")
-    public ApiResult<AppSetting> getAppSetting(@RequestBody ApiBaseRequest request, HttpServletResponse httpServletResponse) {
-        ApiResult<AppSetting> apiResult = new ApiResult<>();
-        apiResult.setSuccess(true);
-        return apiResult;
-    }
-
-    @PostMapping(value = "/candidateProfile.json")
-    public ApiResult<CandidateProfile> getCandidateProfile(@RequestBody ApiBaseRequest request, HttpServletResponse httpServletResponse) {
-        ApiResult<CandidateProfile> apiResult = new ApiResult<>();
-        apiResult.setSuccess(true);
-        return apiResult;
-    }
-
-    @PostMapping(value = "/news.json")
-    public ApiResult<List<SimpleNews>> getNews(@RequestBody ApiBaseRequest request, HttpServletResponse httpServletResponse) {
-        ApiResult<List<SimpleNews>> apiResult = new ApiResult<>();
-        apiResult.setSuccess(true);
-        return apiResult;
-    }
-
-    @PostMapping(value = "/memberProfile.json")
-    public ApiResult<MemberProfile> getMemberProfile(@RequestBody ApiBaseRequest request, HttpServletResponse httpServletResponse) {
-        ApiResult<MemberProfile> apiResult = new ApiResult<>();
-        return apiResult;
-    }
-
-    @PostMapping(value = "/memberLogin.json")
-    public ApiResult<MemberLoginResult> getMemberLogin(@RequestBody MemberLoginRequest request, HttpServletResponse httpServletResponse) {
-        ApiResult<MemberLoginResult> apiResult = new ApiResult<>();
-        apiResult.setSuccess(true);
-        return apiResult;
-    }
-
-    @PostMapping(value = "/memberRegister.json")
-    public ApiResult<BizMemberInfo> registerMember(@RequestBody MemberRegisterRequest request, HttpServletResponse response) {
-
-        return null;
-    }
-
-    @PostMapping(value = "/seqGenerate.json")
-    public ApiResult<String> seqGenerate(@RequestBody ApiRequest request, HttpServletResponse response) {
-        return null;
-    }
 
     @PostMapping(value = "/sample.json")
     private ApiResult<String> getSample(@RequestBody ApiRequest request, HttpServletResponse response) {
@@ -91,12 +36,26 @@ public class ApiController extends AppController {
         });
     }
 
+    @GetMapping(value = "/generateKeyIdx.php")
+    private String getKeyIdx(@RequestBody String request) {
+        String allChars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789.";
 
-    @Autowired
-    private CacheService cacheService;
+        List<String> indexes = new ArrayList<>();
 
-    @GetMapping("/reloadCache")
-    public List<String> reloadCache() {
-        return cacheService.refreshAllCache();
+        String expectStr = request;
+        for (char item : expectStr.toCharArray()) {
+            int index = allChars.indexOf(String.valueOf(item));
+            indexes.add(String.valueOf(index));
+        }
+
+        String response = String.join(",", indexes);
+        response += " - " + indexes.size();
+
+        return response;
+    }
+
+    @GetMapping(value = "sample.php")
+    private String getSample() {
+        return "EzService Sample Response";
     }
 }
