ca515d1709cadc0dc2a97820a9643a798b634fa9
Heri Wijoyo <heri.wijoyo@gmail.com>
2024-03-25 05:00:14 +0700

    add update sorting logic

--- app/biz/arahindonesia/src/main/java/id/ezclouds/biz/ezservice/service/dataservice/AppImageGalleryService.java
+++ app/biz/arahindonesia/src/main/java/id/ezclouds/biz/ezservice/service/dataservice/AppImageGalleryService.java
@@ -127,8 +127,17 @@ public class AppImageGalleryService {
         AppImageGalleryDO appImageGalleryDO = appImageGalleryRepository.findByIdAndOrgId(itemId, orgId);
         AssertUtil.notNull(appImageGalleryDO, EzErrorCode.DATA_NOT_FOUND);
 
+        if (StringUtil.equals(section, "status")) {
+            appImageGalleryDO.setStatus(Integer.parseInt(value));
+        }
         if (StringUtil.equals(section, "home")) {
             appImageGalleryDO.setFlagHomeSlide(Integer.parseInt(value));
+        }
+        if (StringUtil.equals(section, "portfolio")) {
+            appImageGalleryDO.setFlagPortfolioSlide(Integer.parseInt(value));
+        }
+        if (StringUtil.equals(section, "sorting")) {
+            appImageGalleryDO.setSorting(Integer.parseInt(value));
         }
         appImageGalleryRepository.saveAndFlush(appImageGalleryDO);
         return appImageGalleryDO.getId();
