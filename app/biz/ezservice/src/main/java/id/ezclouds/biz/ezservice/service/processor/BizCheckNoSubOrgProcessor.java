e0290f30fc2acef3b84083e73bb3a96273d54e36
Heri Wijoyo <heri.wijoyo@gmail.com>
2024-07-21 21:52:15 +0700

    refactor processor with new abstract template

--- app/biz/ezservice/src/main/java/id/ezclouds/biz/ezservice/service/processor/BizCheckNoSubOrgProcessor.java
+++ app/biz/ezservice/src/main/java/id/ezclouds/biz/ezservice/service/processor/BizCheckNoSubOrgProcessor.java
@@ -8,10 +8,10 @@ import id.ezclouds.biz.ezservice.enums.BizConnectType;
 import id.ezclouds.biz.ezservice.service.processor.shared.BizThreadSharedResource;
 import id.ezclouds.biz.ezservice.service.processor.event.BizProcessEvent;
 import id.ezclouds.biz.ezservice.service.inner.service.BizConnectInnerService;
-import id.ezclouds.biz.ezservice.service.template.BizProcessTemplate;
 import id.ezclouds.common.util.DateUtil;
 import id.ezclouds.core.member.service.CoreMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -22,8 +22,9 @@ import java.util.*;
  * @version $Id: BizCheckNoSubOrgProcessor.java, v 0.1 2024‐07‐20 1:06 AM Heri Wijoyo (heri.wijoyo@gmail.com) Exp $$
  */
 @Service
+@Scope(value = "prototype")
 @Async
-public class BizCheckNoSubOrgProcessor {
+public class BizCheckNoSubOrgProcessor extends BizAsyncProcessor {
 
     @Autowired
     private BizThreadSharedResource bizThreadSharedResource;
@@ -34,61 +35,49 @@ public class BizCheckNoSubOrgProcessor {
     @Autowired
     private BizConnectInnerService bizConnectInnerService;
 
-    public void process(String orgId) {
-        final List<String> logData = new ArrayList<>();
+    @Override
+    public BizProcessEvent getProcessEvent() {
+        return BizProcessEvent.DAILY_CHECK_NO_SUB_ORG;
+    }
+
+    @Override
+    protected int maxProcessTime() {
+        return 5 * 60 * 1000;
+    }
+
+    @Override
+    protected boolean onProcess(Object request, List<String> logData) {
+        final String orgId = (String) request;
         logData.add("ORG_ID="+ orgId);
 
-        String reportReceiver = "6281281150355";
-        BizProcessTemplate.execute(BizProcessEvent.DAILY_CHECK_NO_SUB_ORG, new BizProcessTemplate.Handler() {
-            @Override
-            public void doStart(BizProcessEvent processEvent) {
+        Date yesterday = DateUtil.getDateAfterDays(new Date(), -1);
+        String startTime = DateUtil.getFormattedDate(yesterday, DateUtil.FORMAT_DATETIME_DEFAULT);
+        Map<String, Long> result = coreMemberService
+                .getEmptyGroupCountByReferrerId(orgId, startTime);
+
+        String reportDate = DateUtil.getFormattedDate(yesterday, DateUtil.FORMAT_DATE);
+        String reportMsg = "Referrer without SubOrg ("+ reportDate +"):\n";
+        if (result.size() < 1) {
+            reportMsg += "- TIDAK ADA";
+        } else {
+            List<String> referrerIds = new ArrayList<>();
+            for (Map.Entry<String, Long> entry : result.entrySet()) {
+                referrerIds.add(entry.getKey());
             }
 
-            @Override
-            public boolean doProcess(BizProcessEvent processEvent) {
-                bizThreadSharedResource.startProcess(processEvent.getEventCode());
-
-                Date yesterday = DateUtil.getDateAfterDays(new Date(), -1);
-                String startTime = DateUtil.getFormattedDate(yesterday, DateUtil.FORMAT_DATETIME_DEFAULT);
-                Map<String, Long> result = coreMemberService
-                        .getEmptyGroupCountByReferrerId(orgId, startTime);
-
-                String reportDate = DateUtil.getFormattedDate(yesterday, DateUtil.FORMAT_DATE);
-                String reportMsg = "Referrer without SubOrg ("+ reportDate +"):\n";
-                if (result.size() < 1) {
-                    reportMsg += "- TIDAK ADA";
-                } else {
-                    List<String> referrerIds = new ArrayList<>();
-                    for (Map.Entry<String, Long> entry : result.entrySet()) {
-                        referrerIds.add(entry.getKey());
-                    }
-
-                    Map<String, String> memberNamesMap = coreMemberService
-                            .getMemberNamesMap(referrerIds);
-                    for (Map.Entry<String, String> namesEntry : memberNamesMap.entrySet()) {
-                        reportMsg += namesEntry.getValue() +" : "+ result.get(namesEntry.getKey()) +"\n";
-                    }
-                }
-
-                bizConnectInnerService.sendMessage(
-                        BizConnectType.WHATSAPP,
-                        orgId,
-                        reportReceiver,
-                        reportMsg
-                );
-
-                return true;
+            Map<String, String> memberNamesMap = coreMemberService
+                    .getMemberNamesMap(referrerIds);
+            for (Map.Entry<String, String> namesEntry : memberNamesMap.entrySet()) {
+                reportMsg += namesEntry.getValue() +" : "+ result.get(namesEntry.getKey()) +"\n";
             }
+        }
 
-            @Override
-            public void doFinish(BizProcessEvent processEvent) {
-                bizThreadSharedResource.stopProcess();
-            }
-
-            @Override
-            public List<String> getLogData() {
-                return logData;
-            }
-        });
+        bizConnectInnerService.sendMessage(
+                BizConnectType.WHATSAPP,
+                orgId,
+                "6281281150355",
+                reportMsg
+        );
+        return true;
     }
 }
