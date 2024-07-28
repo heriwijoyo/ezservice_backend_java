0ba6dfc1aeb67415c80daa490166ef88312c3b20
Heri Wijoyo <heri.wijoyo@gmail.com>
2024-07-28 07:09:38 +0700

    add handler code for core scheduler

--- app/core/process/src/main/java/id/ezclouds/core/process/biz/BizProcessorReportGenerateOverall.java
+++ app/core/process/src/main/java/id/ezclouds/core/process/biz/BizProcessorReportGenerateOverall.java
@@ -4,9 +4,34 @@
  */
 package id.ezclouds.core.process.biz;
 
+import id.ezclouds.core.process.model.BizProcessEvent;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * @author Heri Wijoyo (heri.wijoyo@gmail.com)
  * @version $Id: BizProcessorReportGenerateOverall.java, v 0.1 2024‐07‐28 4:38 AM Heri Wijoyo (heri.wijoyo@gmail.com) Exp $$
  */
-public class BizProcessorReportGenerateOverall {
+@Service
+public class BizProcessorReportGenerateOverall extends BizAsyncProcessor {
+
+    @Override
+    public BizProcessEvent getProcessEvent() {
+        return BizProcessEvent.GENERATE_REPORT_OVERALL;
+    }
+
+    @Override
+    protected int maxProcessTime() {
+        return 1 * 60 * 1000;
+    }
+
+    @Override
+    protected boolean onProcess(Object request, List<String> logData) {
+        String orgId = (String) request;
+        logData.add("ORG_ID="+ orgId);
+
+
+        return true;
+    }
 }
--- app/core/process/src/main/java/id/ezclouds/core/process/scheduler/CoreSchedulerProcessor.java
+++ app/core/process/src/main/java/id/ezclouds/core/process/scheduler/CoreSchedulerProcessor.java
@@ -8,6 +8,10 @@ import id.ezclouds.common.facade.biz.BizSchedulerService;
 import id.ezclouds.common.facade.process.SchedulerProcessor;
 import id.ezclouds.common.model.result.BaseResult;
 import id.ezclouds.common.util.facade.BeanFacadeUtil;
+import id.ezclouds.core.process.biz.BizProcessorReportGenerateOverall;
+import id.ezclouds.core.process.model.CoreSchedulerScene;
+import id.ezclouds.core.process.template.CoreSchedulerTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,9 +21,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class CoreSchedulerProcessor implements SchedulerProcessor {
 
+    @Autowired
+    private BizProcessorReportGenerateOverall bizProcessorReportGenerateOverall;
+
     @Override
     public BaseResult execute(String scene) {
-        BizSchedulerService bizSchedulerService = BeanFacadeUtil.getBean(BizSchedulerService.class);
-        return bizSchedulerService.execute(scene);
+
+        CoreSchedulerScene schedulerScene = CoreSchedulerScene.getByCode(scene);
+
+        switch (schedulerScene) {
+            case UNKNOWN:
+                BizSchedulerService bizSchedulerService = BeanFacadeUtil.getBean(BizSchedulerService.class);
+                return bizSchedulerService.execute(scene);
+            default:
+                return executeCoreScheduler(schedulerScene);
+        }
+    }
+
+    private BaseResult executeCoreScheduler(CoreSchedulerScene schedulerScene) {
+        return CoreSchedulerTemplate.execute(schedulerScene.getCode(), new CoreSchedulerTemplate.Handler() {
+            @Override
+            public void preProcess(CoreSchedulerScene schedulerScene) {
+            }
+
+            @Override
+            public void process(CoreSchedulerScene schedulerScene) {
+                switch (schedulerScene) {
+                    case RJL_DAILY_REPORT_OVERALL:
+                        bizProcessorReportGenerateOverall.process("RJL0");
+                        break;
+                }
+            }
+        });
     }
 }
