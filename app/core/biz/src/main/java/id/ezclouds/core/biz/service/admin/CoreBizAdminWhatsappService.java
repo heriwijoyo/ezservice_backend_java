482b5bd92a751e9fb0a0ca5d1cb3b8ebf343145a
Heri Wijoyo <heri.wijoyo@gmail.com>
2024-08-29 13:15:53 +0700

    update test send message for admin

--- app/core/biz/src/main/java/id/ezclouds/core/biz/service/admin/CoreBizAdminWhatsappService.java
+++ app/core/biz/src/main/java/id/ezclouds/core/biz/service/admin/CoreBizAdminWhatsappService.java
@@ -4,9 +4,23 @@
  */
 package id.ezclouds.core.biz.service.admin;
 
+import id.ezclouds.common.facade.auth.AuthAdminService;
 import id.ezclouds.common.facade.biz.admin.BizAdminWhatsappService;
+import id.ezclouds.common.facade.integration.EzConnectService;
+import id.ezclouds.common.facade.member.MemberBackOfficeService;
+import id.ezclouds.common.facade.template.BizServiceTemplate;
+import id.ezclouds.common.model.auth.AuthAdminSession;
+import id.ezclouds.common.model.auth.AuthRole;
+import id.ezclouds.common.model.integration.WhatsappSendRequest;
+import id.ezclouds.common.model.member.MemberBackOffice;
+import id.ezclouds.common.model.message.CommonMessageConstant;
 import id.ezclouds.common.model.request.admin.WebBizUpdateRequest;
 import id.ezclouds.common.model.result.BizResult;
+import id.ezclouds.common.util.assertion.AssertUtil;
+import id.ezclouds.common.util.exception.BizErrorMessageHelper;
+import id.ezclouds.common.util.exception.EzErrorCode;
+import id.ezclouds.common.util.exception.EzErrorException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,8 +30,50 @@ import org.springframework.stereotype.Service;
 @Service
 public class CoreBizAdminWhatsappService implements BizAdminWhatsappService {
 
+    @Autowired
+    private AuthAdminService authAdminService;
+
+    @Autowired
+    private MemberBackOfficeService memberBackOfficeService;
+
+    @Autowired
+    private EzConnectService ezConnectService;
+
     @Override
     public BizResult sendMessage(WebBizUpdateRequest<String> request) {
-        return null;
+        final BizResult bizResult = new BizResult();
+        BizServiceTemplate.execute(request, bizResult, new BizServiceTemplate.Handler() {
+            @Override
+            public void onRequestCheck() throws EzErrorException {
+                AssertUtil.notNull(request, EzErrorCode.ILLEGAL_PARAM);
+                AssertUtil.notBlank(request.getSessionId(), EzErrorCode.ILLEGAL_PARAM);
+                AssertUtil.notBlank(request.getObject(), EzErrorCode.ILLEGAL_PARAM);
+            }
+
+            @Override
+            public void onBizProcess() throws Exception {
+                AuthAdminSession session = authAdminService
+                        .authenticateAdminSession(request.getSessionId());
+                authAdminService.authorizeSessionForRole(session, AuthRole.ADMIN_ORG);
+
+                MemberBackOffice member = memberBackOfficeService
+                        .getMemberDetail(session.getMemberId());
+
+                WhatsappSendRequest sendRequest = new WhatsappSendRequest();
+                sendRequest.setOrgId(session.getOrgId());
+                sendRequest.setMessage(request.getObject());
+                sendRequest.setPhoneNumber(member.getPhone());
+                ezConnectService.sendWhatsappMessage(sendRequest);
+
+                bizResult.setSuccess(true);
+                bizResult.setObject(CommonMessageConstant.BIZ_OPERATION_SUCCESS);
+            }
+
+            @Override
+            public String getErrorMessage(EzErrorCode ezErrorCode) {
+                return BizErrorMessageHelper.getBizErrorMessage(ezErrorCode);
+            }
+        });
+        return bizResult;
     }
 }
