036b0e17dc7f932c4f07d79da9d59931c7d69707
Heri Wijoyo <heri.wijoyo@gmail.com>
2024-10-04 23:30:43 +0700

    update

--- app/core/biz/src/main/java/id/ezclouds/core/biz/service/report/accumulate/BizReportAccumulateVoterRegister.java
+++ app/core/biz/src/main/java/id/ezclouds/core/biz/service/report/accumulate/BizReportAccumulateVoterRegister.java
@@ -4,22 +4,63 @@
  */
 package id.ezclouds.core.biz.service.report.accumulate;
 
+import id.ezclouds.common.facade.area.CoreAreaScanListener;
+import id.ezclouds.common.facade.area.CoreAreaService;
+import id.ezclouds.common.facade.area.CoreWorkingAreaService;
+import id.ezclouds.common.facade.config.CoreConfigService;
+import id.ezclouds.common.model.area.CoreArea;
+import id.ezclouds.common.model.area.CoreAreaLevel;
+import id.ezclouds.common.model.biz.election.BizVoter;
+import id.ezclouds.common.model.config.CoreOrgConfigType;
 import id.ezclouds.common.model.process.ProcessStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 /**
  * @author Heri Wijoyo (heri.wijoyo@gmail.com)
  * @version $Id: BizReportAccumulateVoterRegister.java, v 0.1 2024‐10‐02 3:13 AM Heri Wijoyo (heri.wijoyo@gmail.com) Exp $$
  */
 @Service
+@Scope("prototype")
 @Qualifier("bizReportAccumulateVoterRegister")
 public class BizReportAccumulateVoterRegister implements ReportAccumulateProcessor {
 
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+
+    private CoreConfigService coreConfigService;
+
+    private CoreAreaService coreAreaService;
+
+    private CoreWorkingAreaService coreWorkingAreaService;
+
     @Override
     public void process(String orgId, Object payload, ReportAccumulateProcessHandler handler) {
+        BizVoter bizVoter = (BizVoter) payload;
 
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+
+                coreWorkingAreaService.scanWorkingAreaRecursive(orgId, CoreAreaLevel.VILLAGE, new CoreAreaScanListener() {
+                    @Override
+                    public void areaOnTargetLevel(CoreArea currentArea) {
+                        processOnAreaLevel(currentArea, orgId, bizVoter);
+                    }
+                });
+
+            }
+        });
 
         handler.onFinished(ProcessStatus.EXCEPTION);
+    }
+
+    private void processOnAreaLevel(CoreArea coreArea, String orgId, BizVoter bizVoter) {
+
     }
 }
