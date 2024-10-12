8ceeac254faf8fc343199354160e7cb40e99e7d2
Heri Wijoyo <heri.wijoyo@gmail.com>
2024-10-12 08:45:21 +0700

    update

--- app/core/process/src/main/java/id/ezclouds/core/process/migration/CoreProcessMigrateMember.java
+++ app/core/process/src/main/java/id/ezclouds/core/process/migration/CoreProcessMigrateMember.java
@@ -55,7 +55,7 @@ public class CoreProcessMigrateMember extends BizAsyncProcessor {
             }
 
             try {
-                Thread.sleep(500);
+                Thread.sleep(100);
             } catch (Exception ignored) {}
 
             coreMembers.clear();
