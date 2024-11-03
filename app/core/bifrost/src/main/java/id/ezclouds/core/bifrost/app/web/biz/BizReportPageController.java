73d153c758d881130cae29fe4d94be93180ac652
Heri Wijoyo <heri.wijoyo@gmail.com>
2024-11-03 09:36:08 +0700

    update

--- app/core/bifrost/src/main/java/id/ezclouds/core/bifrost/app/web/biz/BizReportPageController.java
+++ app/core/bifrost/src/main/java/id/ezclouds/core/bifrost/app/web/biz/BizReportPageController.java
@@ -41,12 +41,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
